7235649c050577983d2a2c8031c5ed5b2e103914
Frank Rammacher <info@symfony-lernen.de>
2020-01-22 08:06:47 +0100

    Composer file added for PHPUnit purpose, autoloader.php removed because of new autoload.php added by composer

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once __DIR__ . '/../vendor/autoloader.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new ScoolingDesignPattern\App();
 $app->run();
